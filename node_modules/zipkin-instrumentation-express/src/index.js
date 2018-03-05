38030bc95c88d0a2ac70d826e5fb19fc28a858b4
Raghav Suryanarayanan <Raghav.suryanarayanan@bglgroup.co.uk>
2018-03-05 09:13:20 +0000

    first commit

--- node_modules/zipkin-instrumentation-express/src/index.js
+++
@@ -1,7 +0,0 @@
-const expressMiddleware = require('./expressMiddleware');
-const wrapExpressHttpProxy = require('./wrapExpressHttpProxy');
-
-module.exports = {
-  expressMiddleware,
-  wrapExpressHttpProxy
-};
--- node_modules/zipkin-transport-http/index.d.ts
+++
@@ -1,7 +0,0 @@
-import {JsonEncoder, Logger, model} from "zipkin"
-
-declare class HttpLogger implements Logger {
-  constructor(options: {endpoint: string, httpInterval?: number, jsonEncoder?: JsonEncoder, httpTimeout?: number});
-  logSpan(span: model.Span): void;
-}
-export {HttpLogger}
